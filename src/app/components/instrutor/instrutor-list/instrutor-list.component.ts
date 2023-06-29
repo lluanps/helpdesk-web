84a6286657c09388544d5ba1adf206f6ed3d470d
lluanps <lluanps@gmail.com>
2023-06-29 02:46:57 -0300

    Criado tabela para instrutores e povoando elas com informações temporarias

--- src/app/components/instrutor/instrutor-list/instrutor-list.component.ts
+++ src/app/components/instrutor/instrutor-list/instrutor-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Instrutor } from 'src/app/models/instrutor';
 
 @Component({
   selector: 'app-instrutor-list',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstrutorListComponent implements OnInit {
 
+  ELEMENT_DATA: Instrutor[] = [
+    {    
+      id: 1,
+      nome: 'Luan Pinheiro',
+      cpf: '123.456.789-10',
+      email: 'luan@gmail.com',
+      senha: 'admin',
+      perfis: ['0'],
+      dataCriacao: '29/06/2023'
+    }
+  ];
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Instrutor>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
