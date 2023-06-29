2b51d7ada4f3c85e53da43c125e976809bd2e3b0
lluanps <lluanps@gmail.com>
2023-06-29 02:09:50 -0300

    Componente instrutor-list Criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,15 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { InstrutorListComponent } from './components/instrutor/instrutor-list/instrutor-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    InstrutorListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/instrutor/instrutor-list/instrutor-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instrutor-list',
+  templateUrl: './instrutor-list.component.html',
+  styleUrls: ['./instrutor-list.component.css']
+})
+export class InstrutorListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
