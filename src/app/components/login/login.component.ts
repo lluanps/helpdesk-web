8f2f910fe5faa60339af40342c5cc221f1c675e4
lluanps <lluanps@gmail.com>
2023-06-30 04:26:14 -0300

    Validação de formulario de Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  creds?: Credenciais =  {
+    email: '',
+    senha: ''
+  }
+
+  // verifica email
+  email = new FormControl(null, Validators.email);
+  // verifica senha 
+  senha = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean {
+    if (this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+ 
 }
