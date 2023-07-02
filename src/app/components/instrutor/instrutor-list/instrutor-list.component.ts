4b3217231ceaf70bd40acb93df0da984ea38dfd2
lluanps <lluanps@gmail.com>
2023-07-02 16:27:36 -0300

    Finalizando componente instrutor-list

--- src/app/components/instrutor/instrutor-list/instrutor-list.component.ts
+++ src/app/components/instrutor/instrutor-list/instrutor-list.component.ts
@@ -34,5 +34,13 @@ export class InstrutorListComponent implements OnInit {
     });
   }
 
-}
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
 
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+}
