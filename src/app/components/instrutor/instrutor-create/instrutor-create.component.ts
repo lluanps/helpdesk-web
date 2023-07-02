4c62b7af2d60397e07753538225868eda10c9eab
lluanps <lluanps@gmail.com>
2023-07-02 20:41:38 -0300

    Validação de formularios Instrutor-create

--- src/app/components/instrutor/instrutor-create/instrutor-create.component.ts
+++ src/app/components/instrutor/instrutor-create/instrutor-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-instrutor-create',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstrutorCreateComponent implements OnInit {
 
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, [Validators.required, Validators.maxLength(11)]);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean {
+    return this.nome.valid &&  this.cpf.valid && this.email.valid &&  this.senha.valid
   }
 
 }
