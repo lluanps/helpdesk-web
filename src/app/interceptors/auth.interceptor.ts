f09729be20460fbb3e383e5a9664733577e0eacf
lluanps <lluanps@gmail.com>
2023-07-01 23:36:36 -0300

    Interceptor para requisições HTTP

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,35 @@
+/*ng g interceptor pasta/nome_arquivo*/ 
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let token = localStorage.getItem('token');
+    if (token) {
+      const cloneReq = 
+      request.clone({ headers: request.headers.set('Authorizattion', `Bearer ${token}`)});
+      return next.handle(cloneReq);
+    } else {
+      return next.handle(request);
+    }
+  }
+}
+
+export const AuthInterceptorProvider = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  }
+]
