42d4ab68385a5667d28301e98eda17c8e7ad274a
lluanps <lluanps@gmail.com>
2023-07-03 00:14:38 -0300

    metodo para criar novo instrutor

--- src/app/components/instrutor/instrutor-create/instrutor-create.component.ts
+++ src/app/components/instrutor/instrutor-create/instrutor-create.component.ts
@@ -1,5 +1,9 @@
+import { Instrutor } from 'src/app/models/instrutor';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { InstrutorService } from 'src/app/services/instrutor.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-instrutor-create',
@@ -8,18 +12,60 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class InstrutorCreateComponent implements OnInit {
 
+  instrutor: Instrutor = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCriacao: ''
+  }
+
   nome: FormControl = new FormControl(null, Validators.minLength(3));
   cpf: FormControl = new FormControl(null, [Validators.required, Validators.maxLength(11)]);
   email: FormControl = new FormControl(null, Validators.email);
   senha: FormControl = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(
+    private service: InstrutorService,
+    private toastr: ToastrService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  create(): void {
+    this.service.create(this.instrutor).subscribe(() => {
+      this.toastr.success('Instrutor cadastrado com sucesso', 'Cadastro')
+      this.router.navigate(['intrutores']);
+    }, ex => {
+      if (ex.error.errors) {
+        ex.error.errors.forEach(element => {
+            this.toastr.error(element.message);
+        });
+      }else {
+         this.toastr.error(ex.error.message);
+      }
+    });
+  }
+
+  addPerfil(perfil: any): void {
+    // se clicar em um perfil ja selecionado, ele remove, caso contrario seleciona
+    if (this.instrutor.perfis.includes(perfil)) {
+      this.instrutor.perfis.splice(this.instrutor.perfis.indexOf(perfil), 1);
+      console.log(this.instrutor.perfis);
+    } else {
+      this.instrutor.perfis.push(perfil);
+      console.log(this.instrutor.perfis);
+    }
+
   }
 
   validaCampos(): boolean {
     return this.nome.valid &&  this.cpf.valid && this.email.valid &&  this.senha.valid
   }
+
 
 }
--- src/app/services/instrutor.service.ts
+++ src/app/services/instrutor.service.ts
@@ -1,8 +1,8 @@
+import { Instrutor } from './../models/instrutor';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_CONFIG } from '../config/api.config';
-import { Instrutor } from '../models/instrutor';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,10 @@ export class InstrutorService {
 
   findAll(): Observable<Instrutor[]> {
     return this.http.get<Instrutor[]>(`${API_CONFIG.baseUrl}instrutores`)
+  }
+
+  create(instrutor: Instrutor): Observable<Instrutor> {
+    return this.http.post<Instrutor>(`${API_CONFIG.baseUrl}instrutores/save`, instrutor);
   }
 
 }
